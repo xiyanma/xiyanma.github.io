c5075035743cb337c5ce768aa3ee3aaa99384364
xiyanma <2577014618@qq.com>
2022-06-10 16:19:10 +0800

    patch:修复路由

--- config/menus/index.ts
+++ config/menus/index.ts
@@ -1,18 +1,18 @@
-export default {
-  '/guide': [
-    {
-      title: '使用说明',
-      path: '/README',
-    },
-  ],
-  '/socket-manager': [
-    {
-      title: 'socket-manager',
-      children: ['SocketManager'],
-    },
-    {
-      title: 'time',
-      children: ['time/index'],
-    },
-  ],
-};
+// export default {
+//   '/guide': [
+//     {
+//       title: '使用说明',
+//       path: '/README',
+//     },
+//   ],
+//   '/socket-manager': [
+//     {
+//       title: 'socket-manager',
+//       children: ['SocketManager'],
+//     },
+//     {
+//       title: 'time',
+//       children: ['time/index'],
+//     },
+//   ],
+// };
