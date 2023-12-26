5aacef4ed14a52b0e1b72891c0706c162fd7a6e7
xiyan <37499101+xiyanma@users.noreply.github.com>
2023-12-26 11:31:33 +0800

    feat: 使用范型

--- packages/socket-manager/src/SocketManager.ts
+++ packages/socket-manager/src/SocketManager.ts
@@ -1,57 +1,68 @@
 import { v4 as uuid } from 'uuid';
 import { parseJson } from './util';
 
-class SocketManager {
-  socket;
-  responseMap: {
-    [key: string]: {
-      resolve: (value: unknown) => void;
-      reject: (value: unknown) => void;
-      data?: any;
-    };
-  } = {};
+interface ResponseMapEntry {
+  resolve: (value: unknown) => void;
+  reject: (value: unknown | string) => void;
+  data?: Record<string, any>;
+}
 
-  constructor(socket: any) {
+interface ApiEvent {
+  reqId: string;
+  data: string;
+}
+
+interface ApiFailure {
+  reqId: string;
+}
+
+class Socket {
+  emit(event: string, request: Record<string, any>): void;
+  on(event: string, f: Function): void;
+  disconnect(): void;
+}
+
+class SocketManager {
+  socket: Socket;
+  responseMap: Record<string, ResponseMapEntry> = {};
+
+  constructor(socket: Socket) {
     this.socket = socket;
   }
 
-  send(data: any) {
+  send(data: Record<string, any>): Promise<unknown> {
     const reqId = uuid();
-    const req = new Promise((resolve, reject) => {
-      //如果你想保存返回的data
+    const req = new Promise<unknown>((resolve, reject) => {
       if (data?.type === 'getPrinters') {
         this.responseMap[reqId] = { resolve, reject, data };
       } else {
         this.responseMap[reqId] = { resolve, reject };
       }
+
       this.socket.emit('api', {
         ...data,
         reqId,
       });
     });
+
     return req;
   }
 
-  // 监听结果返回
-  listening(data: { port: any }) {
-    /* 通用：对返回结果的处理*/
-    this.socket.on('apiSuccess', (event: any) => {
+  listening(): void {
+    this.socket.on('apiSuccess', (event: ApiEvent) => {
       try {
-        // 请求成功
         const { reqId, data } = event;
         const { resolve } = this.responseMap[reqId];
         return resolve(data ? parseJson(data) : '');
       } catch (e) {
-        // 请求成功-但返回结果失败
         const { reqId, data } = event;
         const { reject } = this.responseMap[reqId];
         return reject(data ? parseJson(data) : '');
       }
     });
 
-    this.socket.on('apiFail', (error: any) => {
+    this.socket.on('apiFail', (error: ApiFailure) => {
       const reqBody = this.responseMap[error?.reqId]?.data;
-      //请求失败
       const { reqId } = error;
       const { reject } = this.responseMap[reqId];
       try {
@@ -63,7 +74,7 @@ class SocketManager {
     });
   }
 
-  disconnect() {
+  disconnect(): void {
     this.socket.disconnect();
     Object.keys(this.responseMap).forEach((reqId) => {
       this.responseMap[reqId].reject('disconnect');
